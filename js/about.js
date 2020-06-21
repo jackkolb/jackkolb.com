db50db78961e447aeb8be4301421446d81535782
Jack Kolb <jackhunterkolb@gmail.com>
2020-06-21 16:36:29 -0700

    changed to light mode, with a dark mode function

--- js/about.js
+++ js/about.js
@@ -11,68 +11,76 @@ function handleAbout() {
 
 
 function loadAbout() {
-	var majenta_color = "<span style='color:#ff33cc'>"
-	var green_color = "<span style='color:#5cd65c'>"
-	var gold_color = "<span style='color:#ffcc66'>"
-	var red_color = "<span style='color:#dd0000'>"
-	var blue_color = "<span style='color:#4477ff'>"
+	var majenta_color = "<span style='color:#ff33cc'>";
+	var green_color = "<span style='color:#4ac44a'>";
+	var gold_color = "<span style='color:#f4c15a'>";
+	var tech_gold_color = "<span style='color:#b3a369'>";
+	var red_color = "<span style='color:#dd0000'>";
+	var blue_color = "<span style='color:#4477ff'>";
+
+	var link_ucr = "<a href='https://ucr.edu' target='_blank'>";
+	var link_ucr_me = "<a href='https://www.me.ucr.edu' target='_blank'>";
+
+	var link_robosub = "<a href='https://robosub.ucr.edu' target='_blank'>";
+	var link_gatech = "<a href='https://gatech.edu' target='_blank'>";
+	var link_gatech_robotics = "<a href='https://robotics.gatech.edu' target='_blank'>";
+	var link_reslife = "<a href='https://housing.ucr.edu/residential-life' target='_blank'>";
+	var link_ieee = "<a href='https://ieeeucr.org' target='_blank'>";
+
+	var link_asme = "<a href='https://asmeucr.com' target='_blank'>";
+	var link_acm = "<a href='https://acmucr.org' target='_blank'>";
+	var link_nrhh = "<a href='https://nrhh.ucr.edu/home.html' target='_blank'>";
+	var link_honors = "<a href='https://honors.ucr.edu' target='_blank'>";
 
 	var about = document.createElement("div");
 		about.id = "about";
 		about.className = "about-box"
-	
-	var school = document.createElement("div");
-		school.className = "school-about";
-		school.innerHTML = "University of California, Riverside";
 
-	var major = document.createElement("div");
-		major.className = "major-about";
-		major.innerHTML = "Mechanical Engineering (2020)<br><br>";
+	var education_intro = document.createElement("div");
+		education_intro.className = "intro-about";
+	    education_intro.innerHTML = "I am a first-year " + blue_color + link_gatech_robotics + "<b>Robotics</b></a></span> PhD student at " + tech_gold_color + link_gatech + "<b>Georgia Tech</b></a></span>. ";
+        education_intro.innerHTML = education_intro.innerHTML + "My interests are in " + green_color + "<b>artificial intelligence</b></span>, " + majenta_color + "<b>software engineering</b></span>, " + red_color + "<b>robotics</b></span>, and " + blue_color + "<b>entrepreneurship</b></span>.<br><br>";
+
+	var undergrad = document.createElement("div");
+		undergrad.className = "org-title";
+		undergrad.innerHTML = "I received a bachelors degree in " + red_color + link_ucr_me + "<b>mechanical engineering</b></a></span> from the " + blue_color + link_ucr + "<b>University of California</b></span> at " + gold_color + "<b>Riverside</b></a></span>.<br><br>";
+
+	var work_title = document.createElement("div");
+		work_title.className = "org-title";
+		work_title.innerHTML = "While at " + blue_color + link_ucr + "<b>UCR</b></a></span> I was involved as:<br>";
 
 	var work_title_1 = document.createElement("div");
 		work_title_1.className = "intro-about";
-		work_title_1.innerHTML = majenta_color + "Project Lead</span> for " + blue_color + "UCR RoboSub</span><br>";	  			
+		work_title_1.innerHTML = "&emsp;- " + majenta_color + "<b>Project Lead</b></span> for " + blue_color + link_robosub + "<b>UCR RoboSub</b></a></span><br>";	  			
 
 	var work_title_2 = document.createElement("div");
 		work_title_2.className = "intro-about";
-		work_title_2.innerHTML = green_color + "Resident Advisor</span> for " + red_color + "UCR Residential Life</span><br>";
+		work_title_2.innerHTML = "&emsp;- " + green_color + "<b>Resident Advisor</b></span> for " + red_color + link_reslife + "<b>UCR Residential Life</b></a></span><br>";
 
 	var work_title_3 = document.createElement("div");
 		work_title_3.className = "intro-about";
-		work_title_3.innerHTML = blue_color + "Projects Coordinator</span> for " + gold_color + "IEEE at UCR</span><br>";
+		work_title_3.innerHTML = "&emsp;- " + blue_color + "<b>Projects Coordinator</b></span> for " + gold_color + link_ieee + "<b>IEEE at UCR</b></a></span><br>";
 
-	var org_title = document.createElement("div");
-	    org_title.className = "org-title";
-	    org_title.innerHTML = "My interests are in " + blue_color + "robotics</span>, " + majenta_color + "software engineering</span>, " + green_color + "AI</span>, and " + red_color + "entrepreneurship</span>. I am actively involved in:<br><br>"
-
-	var orgs = generateOrgs();
+	var orgs = document.createElement("div");
+		orgs.className = "intro-about";
+		orgs.innerHTML = "&emsp;- and with " + red_color + link_asme + "<b>ASME</b></a></span>, " + blue_color + link_acm + "<b>ACM</b></a></span>, " + green_color + link_nrhh + "<b>NRHH</b></a></span>, and " + majenta_color + link_honors + "<b>Honors</b></a></span><br>";
 
 	var intro = document.createElement("div");
 	    intro.className = "intro-about";
-	    intro.innerHTML = "<br><br>When not in class, I can be found coding, RoboSubing, playing piano, attending student org meetings and campus events, sitting in on lectures, and learning new languages... probably coding. Check out the projects section!<br><br>"
+	    intro.innerHTML = "<br><br>I spend most of my time working on side projects, playing piano, sitting in on lectures, and learning new languages. Check out the projects section!<br><br>"
     
     var breakpoint = document.createElement("br");
 
-	var courses = document.createElement("div");
-
-	var me_courses = generateMECourses();
-	//courses.append(me_courses);
-	var cs_courses = generateCSCourses();
-	//courses.append(cs_courses);
-
-	about.append(breakpoint);
-    about.append(breakpoint);
-    about.append(school);
-	about.append(major);
-    about.append(breakpoint);
+	about.append(education_intro);
+    about.append(undergrad);
+	about.append(work_title);
 	about.append(work_title_1);
 	about.append(work_title_2);
 	about.append(work_title_3);
-	about.append(breakpoint);
-    about.append(org_title);
 	about.append(orgs);
+
+	
 	about.append(intro);
-	about.append(courses);
 
 	document.body.insertBefore(about, document.getElementById("content"));
 
--- js/utility.js
+++ js/utility.js
@@ -9,11 +9,48 @@ function removeAll() {
 }
 
 function showBar(name) {
-	document.getElementById(name).setAttribute("style", "margin: 10px; border-bottom: 1px solid white");
+	var color = "green";
+	if (name == "about-button") {
+		color = "#4477ff";
+	}
+	if (name == "projects-button") {
+		color = "#f4c15a";
+	}
+	document.getElementById(name).setAttribute("style", "margin: 10px; border-bottom: 3px solid " + color);
 	return;
 }
 
 function hideBar(name) {
 	document.getElementById(name).setAttribute("style", "margin: 10px");
 	return;
+}
+
+function switchColorMode() {
+	if (!(document.getElementById("about-button").classList.contains("dark"))) {
+		document.body.classList.add('dark-body');
+
+		document.getElementById("name-title").classList.add('dark');
+		document.getElementById("welcome-title").classList.add('dark');
+		document.getElementById("github-icon").setAttribute("fill", "#c9510c");
+		document.getElementById("linkedin-icon").setAttribute("fill", "#007bb6");
+		document.getElementById("facebook-icon").setAttribute("fill", "#3b5998");
+		
+		document.getElementById("about-button").classList.add('dark');
+		document.getElementById("projects-button").classList.add('dark');
+		document.getElementById("resume-button").classList.add('dark');
+		
+	}
+	else {
+		document.body.classList.remove('dark-body');
+
+		document.getElementById("name-title").classList.remove('dark');
+		document.getElementById("welcome-title").classList.remove('dark');
+		document.getElementById("github-icon").setAttribute("fill", "darkslategrey");
+		document.getElementById("linkedin-icon").setAttribute("fill", "darkslategrey");
+		document.getElementById("facebook-icon").setAttribute("fill", "darkslategrey");
+
+		document.getElementById("about-button").classList.remove('dark');
+		document.getElementById("projects-button").classList.remove('dark');
+		document.getElementById("resume-button").classList.remove('dark');
+	}
 }
