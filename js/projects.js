d92f88dd4def34c55d6b24533fa5e4b7bc0e0183
Jack Kolb <jackhunterkolb@gmail.com>
2020-06-22 13:49:50 -0700

    night mode to projects

--- js/projects.js
+++ js/projects.js
@@ -14,9 +14,8 @@ function loadProjects() {
 	fetch('json/projects.json')
 		.then(response => response.text())
 		.then(function(text) {
-			var mydata = JSON.parse(text);
+			var mydata = JSON.parse(text);			
 			var projects = document.createElement("div");
-			projects.append("<br>");
 			projects.id = "projects";
 
 			mydata["projects"].forEach(function(entry) {
@@ -53,18 +52,18 @@ function generateProject(project_name, project_hackathon, project_github, projec
 	var githubLink = document.createElement("a");
 	githubLink.setAttribute("href", project_github);
 	githubLink.setAttribute("target", "_blank");
-	githubLink.innerHTML = "<span style='float:right'><img src='img/github_white.png' height='25'></span>";
+	githubLink.innerHTML = "<span style='float:right'><svg class='project-github-icon' fill='darkslategrey' viewBox='0 0 48 48' width='24' height='24'><path class='st0' d='M23.928 1.15C11 1.15.514 11.638.514 24.566c0 10.343 6.75 19.105 15.945 22.265 1.148.144 1.58-.574 1.58-1.15v-4.02c-6.465 1.436-7.902-3.16-7.902-3.16-1.005-2.73-2.586-3.45-2.586-3.45-2.154-1.435.144-1.435.144-1.435 2.298.144 3.59 2.442 3.59 2.442 2.156 3.59 5.46 2.586 6.753 2.01.142-1.58.86-2.585 1.435-3.16-5.17-.574-10.63-2.585-10.63-11.635 0-2.585.862-4.596 2.442-6.32-.287-.575-1.005-3.017.288-6.177 0 0 2.01-.574 6.464 2.442 1.866-.574 3.877-.718 5.888-.718 2.01 0 4.022.286 5.89.717 4.453-3.016 6.464-2.442 6.464-2.442 1.293 3.16.43 5.602.287 6.177a9.29 9.29 0 0 1 2.44 6.32c0 9.05-5.458 10.918-10.63 11.492.863.718 1.58 2.155 1.58 4.31v6.464c0 .574.432 1.292 1.58 1.15 9.338-3.16 15.946-11.924 15.946-22.266-.143-12.785-10.63-23.27-23.558-23.27z' clip-rule='evenodd' fill-rule='evenodd'/></svg></span>";
 
 	var hackathonTag = document.createElement("div");
 	hackathonTag.className = "project-entry-title";
-	hackathonTag.setAttribute("style", "color:#CC0066");
+	hackathonTag.setAttribute("style", "color:#ff33cc");
 	hackathonTag.innerHTML = "<span><b>[Hackathon]</b></span>";
 
 	var newProjectEntryTools = document.createElement("div");
 	newProjectEntryTools.className = "project-entry-tools";
 	var tools_listing = "";
 	if (project_hackathon == "true") {
-		tools_listing += "<span style='color:#ff33cc'>[Hackathon] </span>"
+		tools_listing += "<span style='color:#ff33cc'><b>[Hackathon] </b></span>"
 	}
 	tools_listing += "<i>" + project_tools + "</i>";
 	newProjectEntryTools.innerHTML = tools_listing;
--- js/utility.js
+++ js/utility.js
@@ -29,12 +29,24 @@ function switchColorMode() {
 	if (!(document.getElementById("about-button").classList.contains("dark"))) {
 		document.body.classList.add('dark-body');
 
+		document.getElementById("content").classList.add('dark');
+
 		document.getElementById("name-title").classList.add('dark');
 		document.getElementById("welcome-title").classList.add('dark');
 		document.getElementById("github-icon").setAttribute("fill", "#c9510c");
 		document.getElementById("linkedin-icon").setAttribute("fill", "#007bb6");
 		document.getElementById("facebook-icon").setAttribute("fill", "#3b5998");
+
+		icons = document.getElementsByClassName("project-github-icon");
+		for (i=0; i<icons.length-1; i++) {
+			icons[i].setAttribute("fill", "#c9510c");
+		}
 		
+		descs = document.getElementsByClassName("project-entry-desc");
+		for (i=0; i<descs.length-1; i++) {
+			descs[i].setAttribute("fill", "whitesmoke");
+		}
+
 		document.getElementById("about-button").classList.add('dark');
 		document.getElementById("projects-button").classList.add('dark');
 		document.getElementById("resume-button").classList.add('dark');
@@ -43,11 +55,23 @@ function switchColorMode() {
 	else {
 		document.body.classList.remove('dark-body');
 
+		document.getElementById("content").classList.add('dark');
+
 		document.getElementById("name-title").classList.remove('dark');
 		document.getElementById("welcome-title").classList.remove('dark');
 		document.getElementById("github-icon").setAttribute("fill", "darkslategrey");
 		document.getElementById("linkedin-icon").setAttribute("fill", "darkslategrey");
 		document.getElementById("facebook-icon").setAttribute("fill", "darkslategrey");
+
+		icons = document.getElementsByClassName("project-github-icon");
+		for (i=0; i<icons.length-1; i++) {
+			icons[i].setAttribute("fill", "darkslategrey");
+		}
+
+		descs = document.getElementsByClassName("project-entry-desc");
+		for (i=0; i<descs.length-1; i++) {
+			descs[i].setAttribute("fill", "#253f3f");
+		}
 
 		document.getElementById("about-button").classList.remove('dark');
 		document.getElementById("projects-button").classList.remove('dark');
