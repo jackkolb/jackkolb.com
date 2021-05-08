f944ba440962e85cd227f3d24da773f3f50a9352
Jack Kolb <jack@jackkolb.com>
2021-05-08 17:27:56 -0400

    updated intro

--- js/about.js
+++ js/about.js
@@ -42,47 +42,31 @@ function loadAbout() {
 
 	var education_intro = document.createElement("div");
 		education_intro.className = "intro-about";
-	    education_intro.innerHTML = "I am a first-year " + blue_color + link_gatech_robotics + "<b>Robotics</b></a></span> PhD student at " + tech_gold_color + link_gatech + "<b>Georgia Tech</b></a></span>, advised by " + majenta_color + link_chernova + "<b>Sonia Chernova</b></a></span>. ";
-		education_intro.innerHTML = education_intro.innerHTML + "My broad interests are in " + green_color + "<b>artificial intelligence</b></span>, " + red_color + "<b>robotics</b></span>, " + blue_color + "<b>entrepreneurship</b></span>, and " + majenta_color + "<b>human-robot interaction</b></span>. ";
-		education_intro.innerHTML = education_intro.innerHTML + "I work in the " + green_color + link_rail + "<b>RAIL Lab</b></a></span>, where I am currently researching how to predict human performance in human+robot teams.<br><br>";
+	    education_intro.innerHTML = "I am a second-year " + blue_color + link_gatech_robotics + "<b>Robotics</b></a></span> PhD student at " + tech_gold_color + link_gatech + "<b>Georgia Tech</b></a></span>, advised by " + majenta_color + link_chernova + "<b>Sonia Chernova</b></a></span>. ";
+		education_intro.innerHTML += "I am part of the " + green_color + link_rail + "<b>RAIL Lab</b></a></span>, where my research uses human cognitive traits to predict performance in human-robot interactive teams. ";
+		education_intro.innerHTML += "More broadly, I am interested in everything " + red_color + "<b>artificial intelligence</b></span>, " + blue_color + "<b>robotics</b></span>, and " + majenta_color + "<b>entrepreneurship</b></span>.";
 
 	var undergrad = document.createElement("div");
 		undergrad.className = "org-title";
-		undergrad.innerHTML = "I received a bachelors degree in " + red_color + link_ucr_me + "<b>mechanical engineering</b></a></span> from the " + blue_color + link_ucr + "<b>University of California</b></span> at " + gold_color + "<b>Riverside</b></a></span>.<br><br>";
+		undergrad.innerHTML = "<br>I received a bachelors degree in " + red_color + link_ucr_me + "<b>mechanical engineering</b></a></span> from the " + blue_color + link_ucr + "<b>University of California</b></span> at " + gold_color + "<b>Riverside</b></a></span>.<br><br>";
 
 	var work_title = document.createElement("div");
 		work_title.className = "org-title";
-		work_title.innerHTML = "While at " + blue_color + link_ucr + "<b>UCR</b></a></span> I was involved as:<br>";
-
-	var work_title_1 = document.createElement("div");
-		work_title_1.className = "intro-about";
-		work_title_1.innerHTML = "&emsp;- " + majenta_color + "<b>Project Lead</b></span> for " + blue_color + link_robosub + "<b>UCR RoboSub</b></a></span><br>";	  			
-
-	var work_title_2 = document.createElement("div");
-		work_title_2.className = "intro-about";
-		work_title_2.innerHTML = "&emsp;- " + green_color + "<b>Resident Advisor</b></span> for " + red_color + link_reslife + "<b>UCR Residential Life</b></a></span><br>";
-
-	var work_title_3 = document.createElement("div");
-		work_title_3.className = "intro-about";
-		work_title_3.innerHTML = "&emsp;- " + blue_color + "<b>Projects Coordinator</b></span> for " + gold_color + link_ieee + "<b>IEEE at UCR</b></a></span><br>";
-
-	var orgs = document.createElement("div");
-		orgs.className = "intro-about";
-		orgs.innerHTML = "&emsp;- and with " + red_color + link_asme + "<b>ASME</b></a></span>, " + blue_color + link_acm + "<b>ACM</b></a></span>, " + green_color + link_nrhh + "<b>NRHH</b></a></span>, and " + majenta_color + link_honors + "<b>Honors</b></a></span><br>";
+		work_title.innerHTML = "While at " + blue_color + link_ucr + "<b>UCR</b></a></span> I was involved with " + blue_color + link_robosub + "<b>RoboSub</b></a></span>, " + red_color + link_reslife + "<b>Residential Life</b></a></span>, and " + gold_color + link_ieee + "<b>IEEE@UCR</b></a></span>.<br>";
 
 	var intro = document.createElement("div");
 	    intro.className = "intro-about";
-	    intro.innerHTML = "<br><br>I spend most of my spare time working on side projects, playing piano, sitting in on lectures, and learning world languages. Check out the projects section!<br><br>"
+	    intro.innerHTML = "<br><br>I spend most of my spare time working on side projects, playing piano, and being involved in student organizations. Check out the projects section!<br><br>"
     
     var breakpoint = document.createElement("br");
 
 	about.append(education_intro);
     about.append(undergrad);
 	about.append(work_title);
-	about.append(work_title_1);
-	about.append(work_title_2);
-	about.append(work_title_3);
-	about.append(orgs);
+	//about.append(work_title_1);
+	//about.append(work_title_2);
+	//about.append(work_title_3);
+	//about.append(orgs);
 
 	
 	about.append(intro);
